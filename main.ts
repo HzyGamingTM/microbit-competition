91330751140d0a95a2dbace5c2148f1dcb1b38c9
HzyGamingTM <60961260+HzyGamingTM@users.noreply.github.com>
2022-09-15 17:50:24 +0800

    Did more shidder

--- main.ts
+++ main.ts
@@ -9,13 +9,17 @@ let Recycling = {
     IS_MOVEMENT: false,
     AMT_RECYCLED: 0,
 
-    detect_movement: () => {
+    init: () => {
+        OLED.init(128, 64);
+        OLED.writeString(`Recycled ${Recycling.AMT_RECYCLED} items.`);
+    }, detect_movement: () => {
         if (pins.digitalReadPin(DigitalPin.P0) == 1) {
             basic.showIcon(IconNames.Happy);
             Recycling.open_bin();
             music.playTone(Recycling.MUSIC_TONE, 100);
         } else basic.showIcon(IconNames.Umbrella);
     }, open_bin: () => {
+        Recycling.AMT_RECYCLED++;
         console.log("opening bin!");
         pins.servoWritePin(AnalogPin.P1, Recycling.OPEN_SPEED)
         control.waitMicros(Recycling.WAIT_TIME);
@@ -23,9 +27,17 @@ let Recycling = {
     }, close_bin: () => {
         console.log("closing bin!");
         pins.servoWritePin(AnalogPin.P1, Recycling.CLOSE_SPEED);
+    },
+    reset_amt_recycled: () => {
+        Recycling.AMT_RECYCLED = 0;
     }
 }
 
+input.onButtonPressed(Button.AB, () => {
+    Recycling.reset_amt_recycled();
+});
+
 basic.forever(() => {
     Recycling.detect_movement();
+    OLED.writeString(`Recycled ${Recycling.AMT_RECYCLED} items.`);
 })
