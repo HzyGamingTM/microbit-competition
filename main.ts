682414ce5a52ccbccb06ce754da8457700d4cb0a
HzyGamingTM <60961260+HzyGamingTM@users.noreply.github.com>
2022-09-15 17:42:43 +0800

    I added stuff

--- main.ts
+++ main.ts
@@ -1,12 +1,31 @@
 // Not using classes [OOP] cuz they bad.
-let Program = {
+let Recycling = {
+    WAIT_TIME: 4000000,
+    MUSIC_TONE: 440,
+
+    CLOSE_SPEED: 0,
+    OPEN_SPEED: 180,
+
+    IS_MOVEMENT: false,
+    AMT_RECYCLED: 0,
+
     detect_movement: () => {
         if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-            music.playTone(440, 100);
-        } else basic.showIcon(IconNames.Angry)
-    },
+            basic.showIcon(IconNames.Happy);
+            Recycling.open_bin();
+            music.playTone(Recycling.MUSIC_TONE, 100);
+        } else basic.showIcon(IconNames.Umbrella);
+    }, open_bin: () => {
+        console.log("opening bin!");
+        pins.servoWritePin(AnalogPin.P1, Recycling.OPEN_SPEED)
+        control.waitMicros(Recycling.WAIT_TIME);
+        Recycling.close_bin();
+    }, close_bin: () => {
+        console.log("closing bin!");
+        pins.servoWritePin(AnalogPin.P1, Recycling.CLOSE_SPEED);
+    }
 }
 
 basic.forever(() => {
-    Program.detect_movement();
+    Recycling.detect_movement();
 })
